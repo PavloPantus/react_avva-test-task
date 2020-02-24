2b440905007eda67a045e270456366ff6738977b
PavloPantus <mr.pantus@gmail.com>
2020-02-25 01:01:48 +0200

    Updates

---
+++ precache-manifest.444e4f73a2d16f23a8a41b9e0eb93863.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acf342043bb9d4e517912e17e8359472",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a7352e60ffa86780673e",
+    "url": "./static/css/main.95147941.chunk.css"
+  },
+  {
+    "revision": "c4f66b5d7e3be719845f",
+    "url": "./static/js/2.b17deee0.chunk.js"
+  },
+  {
+    "revision": "a7352e60ffa86780673e",
+    "url": "./static/js/main.258cca4e.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
